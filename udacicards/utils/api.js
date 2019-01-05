56e2990c68cd30d86ffa58f100fad68f1905a36c
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-06 04:48:59 +1000

    add api functions

--- udacicards/utils/api.js
+++ udacicards/utils/api.js
@@ -5,3 +5,36 @@ export const setDummyData = async () => {
   await AsyncStorage.setItem(DECKS_STORAGE_KEY, JSON.stringify(dummyData));
   return dummyData;
 };
+
+export const getDecks = async () => {
+  const decks = await AsyncStorage.getItem(DECKS_STORAGE_KEY);
+  return JSON.parse(decks);
+};
+
+export const getDeck = id => {
+  return AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    return data[id];
+  });
+};
+
+export const saveDeckTitle = async title => {
+  return await AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({
+      [title]: { title, questions: [] }
+    })
+  );
+};
+export const addCardToDeck = (title, card) => {
+  const decks = AsyncStorage.getItem(DECKS_STORAGE_KEY).then(results => {
+    const data = JSON.parse(results);
+    return data;
+  });
+  const deck = decks[title];
+  deck.questions.push(card);
+  return AsyncStorage.mergeItem(
+    DECKS_STORAGE_KEY,
+    JSON.stringify({ [title]: decks })
+  );
+};
