04f603061d5600b943529698c8361e4e971adb1d
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-06 08:05:22 +1000

    fix reducer to handle adding new card

--- udacicards/reducers/index.js
+++ udacicards/reducers/index.js
@@ -7,8 +7,13 @@ export const decks = (state = {}, action) => {
     case ADD_DECK:
       return { ...state, ...action.deck };
     case ADD_CARD_TO_DECK:
-      const question = { question: action.question, answer: action.answer };
-      return { ...state, ...state[action.title].questions.concat(question) };
+      const card = { question: action.question, answer: action.answer };
+      const questions = Object.assign(
+        [],
+        state[action.title].questions.concat(card)
+      );
+      const deck = { [action.title]: { title: action.title, questions } };
+      return { ...state, deck };
     default:
       return state;
   }
