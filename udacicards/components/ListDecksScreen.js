9f1b7dfc779f9c5c27cca1982722511901586361
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-06 04:51:08 +1000

    Load decks into props

--- udacicards/components/ListDecksScreen.js
+++ udacicards/components/ListDecksScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { connect } from "react-redux";
+import { getDecks } from "../utils/api";
+import { receiveDecks } from "../actions";
 
 export class ListDecksScreen extends React.Component {
   static navigationOptions = {
     title: "Decks"
   };
+  async componentDidMount() {
+    const decks = await getDecks();
+    this.props.dispatch(receiveDecks(decks));
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -31,4 +38,10 @@ export class ListDecksScreen extends React.Component {
   }
 }
 
-export default ListDecksScreen;
+const mapStateToProps = decks => {
+  return {
+    decks
+  };
+};
+
+export default connect(mapStateToProps)(ListDecksScreen);
