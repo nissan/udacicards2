6ac557015ac79bb76319044894aa06166fa1e63c
Nissan Dookeran <nissan.dookeran@gmail.com>
2019-01-06 07:22:31 +1000

    Add new deck styling

--- udacicards/components/NewDeckScreen.js
+++ udacicards/components/NewDeckScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {
   KeyboardAvoidingView,
+  StyleSheet,
   Text,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  Platform
 } from "react-native";
 import { saveDeckTitle } from "../utils/api";
 import { addDeck } from "../actions";
@@ -37,11 +39,10 @@ export class NewDeckScreen extends React.Component {
   render() {
     const { title } = this.state;
     return (
-      <KeyboardAvoidingView
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      >
-        <Text>What is the title of your new deck?</Text>
+      <KeyboardAvoidingView style={styles.container}>
+        <Text style={styles.header}>What is the title of your new deck?</Text>
         <TextInput
+          style={styles.input}
           placeholder="Deck Title"
           onChangeText={this.titleChange}
           value={title}
@@ -53,5 +54,41 @@ export class NewDeckScreen extends React.Component {
     );
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  header: {
+    flexDirection: "row",
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "black",
+    fontSize: 20
+  },
+  input: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 0,
+    marginLeft: 10,
+    marginRight: 10,
+    width: 350,
+    height: 40,
+    color: "black",
+    fontSize: 20,
+    borderRadius: Platform.OS === "ios" ? 16 : 10,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: "rgba(0,0,0,0.24)",
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  }
+});
 export default connect()(NewDeckScreen);
